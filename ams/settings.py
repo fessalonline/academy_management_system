94a87ca45a6b5786a23878f430a89bd53cd7e9f4
Muhammad Faisal <mfessal475@gmail.com>
2022-02-25 20:41:35 +0500

    templates and static directories created and settings.py file updated with templates and staticfiles

--- ams/settings.py
+++ ams/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'ams.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'ams/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'ams/static'
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
